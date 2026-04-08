597b1fae30e3cd043fa062ab6cce084471763cef
SustPanda <91877179+SustPanda@users.noreply.github.com>
2026-04-08 20:47:43 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '')
-
   return {
-    base: env.VITE_BASE_PATH || '/',
+    base: '/lifeos/',
     plugins: [react()],
   }
 })
